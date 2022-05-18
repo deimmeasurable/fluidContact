8c5a5e420c3b4271446a26c39f8b6a3fbe093762
Seun Joshua <sseun976@gmail.com>
2022-05-18 16:53:27 +0000

    first commit

---
+++ src/component/ContactHome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './homePage/Navbar'
+import Sidebar from './homePage/Sidebar'
+
+const HomePage = () => {
+  return (
+    <div style={{flex: 4}}>
+       
+    </div>
+  )
+}
+
+export default HomePage
